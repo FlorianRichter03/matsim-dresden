54e98a2f5372d40590883b38eb1ee32a2b40edf3
FlorianRichter03 <florian.richter.2003@gmail.com>
2026-01-31 18:14:39 +0100

    Reset plus kleine Fehleranpassung in Factory

--- src/main/java/org/matsim/scoring/PersonScoringFunctionFactory.java
+++ src/main/java/org/matsim/scoring/PersonScoringFunctionFactory.java
@@ -2,14 +2,12 @@ package org.matsim.scoring;
 
 import com.google.inject.Inject;
 import org.matsim.api.core.v01.Scenario;
-import org.matsim.api.core.v01.population.Activity;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.scoring.ScoringFunction;
 import org.matsim.core.scoring.ScoringFunctionFactory;
 import org.matsim.core.scoring.SumScoringFunction;
 import org.matsim.core.scoring.functions.*;
-import org.matsim.network.WuerzburgerStrasse_Links;
 
 public class PersonScoringFunctionFactory implements ScoringFunctionFactory {
 	private final Scenario scenario;
@@ -21,8 +19,6 @@ public class PersonScoringFunctionFactory implements ScoringFunctionFactory {
 	PersonScoringFunctionFactory(Scenario scenario) {
 		this.scenario = scenario;
 	}
-
-
 
 
 	@Override
@@ -38,34 +34,13 @@ public class PersonScoringFunctionFactory implements ScoringFunctionFactory {
 		sumScoringFunction.addScoringFunction(new CharyparNagelAgentStuckScoring(params));
 
 		PersonScoring personScoring = new PersonScoring();
-		sumScoringFunction.addScoringFunction(new PersonScoring());
-
-
-		boolean isResident = false;
-		if(person.getSelectedPlan() != null) {
-			isResident = person.getSelectedPlan().getPlanElements().stream()
-				.filter(pe -> pe instanceof Activity)
-				.map(pe -> (Activity) pe)
-				.anyMatch(act ->
-					act.getLinkId() != null &&
-					WuerzburgerStrasse_Links.Links.contains(act.getLinkId())
-						&& (
-						act.getType().startsWith("home")
-							|| act.getType().startsWith("work")
-							|| act.getType().startsWith("edu")
-							|| act.getType().startsWith("shopping")
-							|| act.getType().startsWith("leisure")
-							|| act.getType().startsWith("business")
-							|| act.getType().startsWith("other")));
-		}
-
+		sumScoringFunction.addScoringFunction(personScoring);
 
 		// Register per-person event handler
 		events.addHandler(
-			new BicycleRoadTrafficHandler(person.getId(), personScoring, isResident)
+			new BicycleRoadTrafficHandler(person.getId(), personScoring)
 		);
 
 		return sumScoringFunction;
 	}
 }
-
