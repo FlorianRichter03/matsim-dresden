d64236156b8a0bd46250c937a18d3845b7a2251b
FlorianRichter03 <florian.richter.2003@gmail.com>
2026-01-31 13:59:13 +0100

    aufgeräumt

--- src/main/java/org/matsim/scoring/PersonScoring.java
+++ src/main/java/org/matsim/scoring/PersonScoring.java
@@ -1,20 +1,6 @@
 package org.matsim.scoring;
 
-import org.matsim.api.core.v01.Id;
-import org.matsim.api.core.v01.events.*;
-import org.matsim.api.core.v01.events.handler.ActivityEndEventHandler;
-import org.matsim.api.core.v01.events.handler.LinkLeaveEventHandler;
-import org.matsim.api.core.v01.events.handler.PersonEntersVehicleEventHandler;
-import org.matsim.api.core.v01.events.handler.PersonLeavesVehicleEventHandler;
-import org.matsim.api.core.v01.network.Link;
-import org.matsim.api.core.v01.population.Person;
 import org.matsim.core.scoring.SumScoringFunction;
-import org.matsim.vehicles.Vehicle;
-
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
 
 public class PersonScoring implements SumScoringFunction.BasicScoring {
 
